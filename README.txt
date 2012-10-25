24308851ac26df51672986863ce551cc5ff529a9
blahed <tdunn13@gmail.com>
2012-10-24 20:54:00 -0400

    commit one

---
+++ README.txt
@@ -0,0 +1,16 @@
+=== Forwarded Host URLs ===
+Contributors: blahed
+Tags: plugin, urls, hostname, host, links
+Stable tag: 0.0.1
+License: MIT
+
+Forces WordPress to build urls using the X-Forwarded-Host header, if it exists.
+
+== Description ==
+
+A plugin to force WordPress to build urls based on the X-Forwarded-Host header. Useful if you're viewing a WordPress site using a proxy and the proxy sets the X-Forwarded-Host header (like https://forwardhq.com).
+
+We don't recommend using this in production, but it can be useful for development.
+
+Ideas from https://gist.github.com/949821 and http://odyniec.net/blog/2010/02/wordpress-blog-and-multiple-server-names/
+
---
+++ forwarded-host-urls.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+Plugin Name:    Forwarded Host URLs
+Description:    Forces WordPress to build urls using the X-Forwarded-Host header, if it exists.
+Author:         blahed
+Author URI:     https://github.com/50east
+Plugin URI:     https://github.com/50east/wp-forwarded-host-urls
+Version:        0.0.1
+*/
+
+function has_forwarded_host() {
+  return array_key_exists('HTTP_X_FORWARDED_HOST', $_SERVER);
+}
+
+function forwarded_host() {
+  return $_SERVER['HTTP_X_FORWARDED_HOST'];
+}
+
+function forwarded_base() {
+  $_forwarded_host = forwarded_host();
+  return "//$_forwarded_host";
+}
+
+function apply_forwarded_host() {
+  if ( !has_forwarded_host() )
+    return false;
+  else
+    return forwarded_base();
+}
+
+function replace_with_forwarded_host($url, $path = '') {
+  if ( !has_forwarded_host() )
+    return $url;
+  else
+    return preg_replace('!https?://[a-z0-9.-]*!', '//' . forwarded_host(), $url);
+}
+
+add_filter('pre_option_home', 'apply_forwarded_host');
+add_filter('pre_option_siteurl', 'apply_forwarded_host');
+add_filter('pre_option_url', 'apply_forwarded_host');
+add_filter('stylesheet_uri', 'replace_with_forwarded_host');
+add_filter('admin_url', 'replace_with_forwarded_host');
+
+?>
